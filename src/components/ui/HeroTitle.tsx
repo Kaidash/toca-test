a0bb00f8fa19abb0ac4be63dd2a86436154176f3
Nick Kaidash <kaidash.nikita93@gmail.com>
2025-05-20 11:20:11 +0300

    - raw mobile css

--- src/components/ui/HeroTitle.tsx
+++ src/components/ui/HeroTitle.tsx
@@ -7,10 +7,10 @@ export default function HeroTitle({ title, className }: { title: string, classNa
     <div className={cn('absolute text-foreground py-20 px-4 text-center', className)}>
       <div className="max-w-5xl mx-auto uppercase">
         {!!first && ( <div className="relative flex items-center justify-center my-6">
-          <span className="p-1">
+          <span className="p-1 ">
             <SectionDivider />
           </span>
-          <h1 className="text-6xl md:text-7xl lg:text-[140px] font-serif leading-none">
+          <h1 className="text-5xl md:text-7xl lg:text-[140px] font-serif leading-none">
             {first}
           </h1>
         </div>)
@@ -18,7 +18,7 @@ export default function HeroTitle({ title, className }: { title: string, classNa
 
         {!!second && (
           <div className="relative flex items-center justify-center my-6">
-            <h1 className="text-6xl md:text-7xl lg:text-[140px] font-serif leading-none">
+            <h1 className="text-5xl md:text-7xl lg:text-[140px] font-serif leading-none">
               {second}
             </h1>
             <span className="p-1">
@@ -27,7 +27,7 @@ export default function HeroTitle({ title, className }: { title: string, classNa
           </div>
         )}
 
-        {third && ( <h1 className="text-6xl md:text-7xl lg:text-[140px] font-serif leading-none">
+        {third && ( <h1 className="text-5xl md:text-7xl lg:text-[140px] font-serif leading-none">
           {third}
         </h1>
         )}
